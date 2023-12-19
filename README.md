ed368703f38f5cc50644fd17048cfa497b7ec933
ProdigalP <prodigalprogrammer7@gmail.com>
2023-12-19 23:11:43 +0000

    Project completed. README.md updated. Screenshot added to assets/images folder as opposed to main folder

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# password-generator
+# Password Generator
+
+<img src="assets/images/password-generator.jpg" width="700px">
+
+## Description
+
+In this project I was required to create a random password generator that employees can use in their personal or professional life. There were multiple criteria that allows the user to fine-tune precisely what characters they want featured in the password.
+
+The app has been created primarily using JavaScript with some simple HTML and CSS to make the user experience better.
+
+_Find the link to the the deployed website [here](https://prodigalprogrammer.github.io/Console-Finances/)_
+
+<p align="right">(<a href="#password-generator" >Back to top</a>)</p>
+
+## Usage
+
+Once you open the Console, you will be greeted with a simple text area and button.
+
+Click the "Generate Password" button and an alert will follow prompting you to select a number between 8 and 128. You will not be allowed to select characters other than this number range.
+
+After this you will be prompted to include four different types of characters:
+
+- Special Characters (`!?.-_=+`)
+- Numerical Characters (`036178`)
+- Lower Cased Characters (`qwerty`)
+- Upper Cased Characters (`QWERTY`)
+
+The more types selected, the stronger your password will be. But you must select **at least one** type.
+
+As an example, I can choose to have 18 characters in my password, select only special and numerical characters and will end up with something like the code below :
+
+`5'~~{.{0^?-)-~?)(5`
+
+As you can see there are no characters from the alphabet.
+
+<p align="right">(<a href="#password-generator" >Back to top</a>)</p>
+
+## Features
+
+### Functions
+
+- There were a lot of Function expressions throughout this project and understanding scope as well as calling and declaring of Functions was essential.
+
+### IF & ELSE statements
+
+- I used many IF statements so only certain blocks of code would operate based on the user's choices.
+
+### For Loop
+
+- The For Loop was probably the most important thing used to make this app funtion correctly. It allowed me to quickly extract many random characters from arrays.
+<p align="right">(<a href="#password-generator" >Back to top</a>)</p>
+
+## License
+
+Distributed under the MIT License. See `LICENSE.txt` for more information.
+
+## Contact
+
+Project Link: [https://github.com/prodigalProgrammer/password-generator](https://github.com/prodigalProgrammer/password-generator)
+
+Monrone Stewart: [YouTube](https://www.youtube.com/@ProdigalP) - prodigalprogrammer7@gmail.com
+
+<p align="right">(<a href="#password-generator" >Back to top</a>)</p>
+
+## Acknowledgement
+
+- [GitHub Pages](https://pages.github.com/)
+- [The Complete JavaScript Course 2024: From Zero to Expert!](https://www.udemy.com/course/the-complete-javascript-course/)
+
+<p align="right">(<a href="#password-generator" >Back to top</a>)</p>
--- assets/script.js
+++ assets/script.js
@@ -90,43 +90,62 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  // Parses any value from the 'passwordChars' prompt to become an integer.
   var passwordChars = parseInt(
     prompt(
       "Please select the amount of characters your password will contain: Any number from 8 - 128"
     )
   );
+  // If the 'passwordChars' prompt produces a value between 8 and 128, run this IF statement below.
   if (passwordChars >= 8 && passwordChars <= 128) {
+    // Empty array that character types will be pushed into
     var passwordArray = [];
+    // if the 'specialChars' prompt is true (clicked OK), then the 'specialCharacters' array is pushed into 'passwordArray'.
     var specialChars = confirm(
       "Select OK if you want special characters included"
     );
     if (specialChars) {
+      // Spread operator is used to push the individual indexes of specialCharaters, and not just the array itself.
       passwordArray.push(...specialCharacters);
     }
+    // if the 'numberChars' prompt is true (clicked OK), then the 'numericCharacters' array is pushed into 'passwordArray'.
     var numberChars = confirm(
       "Select OK if you want numerical characters included"
     );
     if (numberChars) {
+      // Spread operator is used to push the individual indexes of numericCharaters, and not just the array itself.
       passwordArray.push(...numericCharacters);
     }
+    // if the 'lowerChars' prompt is true (clicked OK), then the 'lowerCasedCharacters' array is pushed into 'passwordArray'.
     var lowerChars = confirm(
       "Select OK if you want lower-cased characters included"
     );
     if (lowerChars) {
+      // Spread operator is used to push the individual indexes of lowerCasedCharaters, and not just the array itself.
       passwordArray.push(...lowerCasedCharacters);
     }
+    // if the 'upperChars' prompt is true (clicked OK), then the 'upperCasedCharacters' array is pushed into 'passwordArray'.
     var upperChars = confirm(
       "Select OK if you want upper-cased characters included"
     );
     if (upperChars) {
+      // Spread operator is used to push the individual indexes of upperCasedCharaters, and not just the array itself.
       passwordArray.push(...upperCasedCharacters);
     }
-    var passwordAmount = "";
-    for (var i = 0; i < passwordChars; i++) {
-      passwordAmount += getRandom(passwordArray);
+    // IF statement that makes sure that one valid character type must be selected to generate a password.
+    if (!specialChars && !numberChars && !lowerChars && !upperChars) {
+      alert("Please select at least one character type.");
+    } else {
+      // Combining all of the for loop results into a variable named 'passwordAmount'
+      var passwordAmount = "";
+      for (var i = 0; i < passwordChars; i++) {
+        passwordAmount += getRandom(passwordArray);
+      }
+      // Once all characters are combined, the value of 'passwordAmount' is returned.
+      return passwordAmount;
     }
-    return passwordAmount;
   } else {
+    // This ELSE statement prohibits the user from inputting anything else but an integer between 8 and 128.
     alert("Please input a number between 8 and 128 to generate a password.");
   }
 }
@@ -152,7 +171,9 @@ function writePassword() {
   // alert(
   //   `Your Generated Password is: ${password}\n Click OK to copy your password from the text area.`
   // );
-  passwordText.value = password;
+
+  // Nullish coalescing operator stops 'undifined' from being printed if there is no character type selected.
+  passwordText.value = password ?? "";
 }
 
 // Add event listener to generate button
